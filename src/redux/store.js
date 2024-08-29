98094e065d27046f4a7d279a0df703d561b8015f
elisa-sol <elizaveta-2002@bk.ru>
2024-08-29 17:58:02 +0300

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,11 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-// const composeEnhancers =
-//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//         // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
-//       })
-//     : compose;
-
 const store = createStore(
-  rootReducer
-  // , composeEnhancers
+  rootReducer,
+  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
